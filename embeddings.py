6ad34a9d9f87b0a5b27ddd89b44acc40060b4b79
majagrottum <138011685+majagrottum@users.noreply.github.com>
2023-07-10 17:41:08 +0200

    Created functions to retrieve the node embedding and the community labels

--- embeddings.py
+++ embeddings.py
@@ -45,90 +45,79 @@ P = 1
 Q = 0.5
 WL = 300
 
+# Defining a function to retrieve the node embeddings using node2vec
 
-# Precomputing probabilities and generating walks for the healthy cell line
-# Number of workers is set to 1 as it should be smaller than or equal to the number of CPU cores on my laptop which is 2
+def node_embeddings(graph, file_name):
 
-node2vec_h = Node2Vec(G_h, dimensions=D, walk_length=WL, num_walks=10, weight_key='weight', workers=1, p=P, q=Q)  
+    # Precomputing probabilities and generating walks 
+    # Number of workers is set to 1 as it should be smaller than or equal to the number of CPU cores on my laptop which is 2
+    
+    node2vec = Node2Vec(graph, dimensions=D, walk_length=WL, num_walks=10, weight_key='weight', workers=1, p=P, q=Q)  
+    
+    # Generating the node embedding 
+    
+    model = node2vec.fit(window=10, min_count=1, batch_words=4)
+    
+    # Retrieving the node embedding 
+    # model.wv.vectors epresents the embeddings as a two-dimensional array, where each row corresponds to the embedding vector of a specific node.
+    # For example, you can retrieve the embedding vector of a specific node using model.wv.vectors[node_index], where node_index is the numeric index of the node (0-based index).
+    
+    embedding = model.wv.vectors
+    
+    # The embeddings will be saved in the txt file in a space-separated format, with each row representing the embedding vector of a specific node
+    # np.savetxt() will save the file in the current working directory, which is the directory where your Python script or Jupyter Notebook is running
+    
+    np.savetxt(file_name, embedding, delimiter=" ")
 
-# Generating the node embedding for the healthy cell line
-
-model_h = node2vec_h.fit(window=10, min_count=1, batch_words=4)
-
-# Retrieving the node embedding for the healthy cell line
-# model.wv.vectors epresents the embeddings as a two-dimensional array, where each row corresponds to the embedding vector of a specific node.
-# For example, you can retrieve the embedding vector of a specific node using model.wv.vectors[node_index], where node_index is the numeric index of the node (0-based index).
-
-embedding_h = model_h.wv.vectors
-
-# The embeddings will be saved in the txt file in a space-separated format, with each row representing the embedding vector of a specific node
-# np.savetxt() will save the file in the current working directory, which is the directory where your Python script or Jupyter Notebook is running
-
-np.savetxt("embedding_h.txt", embedding_h, delimiter=" ")
+    return embedding
 
 
-# Precomputing probabilities and generating walks for the cancer cell line
-# Number of workers is set to 1 as it should be smaller than or equal to the number of CPU cores on my laptop which is 2
+# Retrieving the node embeddings for the healthy cell line
 
-node2vec_c = Node2Vec(G_c, dimensions=D, walk_length=WL, num_walks=10, weight_key='weight', workers=1, p=P, q=Q) 
+embedding_h = node_embeddings(G_h, "embedding_h.txt")
 
-# Generating the node embedding for the cancer cell line
 
-model_c = node2vec_c.fit(window=10, min_count=1, batch_words=4) 
+# Retrieving the node embeddings for the cancer cell line
 
-# Retrieving the node embedding for the cancer cell line
-# model.wv.vectors epresents the embeddings as a two-dimensional array, where each row corresponds to the embedding vector of a specific node.
-# For example, you can retrieve the embedding vector of a specific node using model.wv.vectors[node_index], where node_index is the numeric index of the node (0-based index).
-
-embedding_c = model_c.wv.vectors
-
-# The embeddings will be saved in the txt file in a space-separated format, with each row representing the embedding vector of a specific node
-# np.savetxt() will save the file in the current working directory, which is the directory where your Python script or Jupyter Notebook is running
-
-np.savetxt("embedding_c.txt", embedding_c, delimiter=" ")
+embedding_c = node_embeddings(G_c, "embedding_c.txt")
 
 
 
     
 # Below follows the community embedding of the networks of the two cell lines using Louvain Community Detection
-# First for the healthy cell line, then for the cancer cell line
+
+
+# Defining a function to retrieve the community labels of each node of the networks
+
+def community_label(graph, file_name):
+
+    # Computing the partition using Louvain community detection
+    # The partition dictionary will contain the community assignments for each node in the graph
+    # The resolution parameter will change the size of the communities
+    # By setting the random_state parameter to the same value each time you run the algorithm, you should obtain the same community assignments
+    
+    partition = community.best_partition(graph, weight='weight', resolution = 1, random_state = 0))
+    
+    # Converting the community_assignments dictionary to a numpy array
+    # Each tuple from partition_h.items() consists of a node as the key and its corresponding community label as the value.
+    # The resulting array will have each tuple as a row, where the first column contains the node and the second column contains the community label.
+    
+    communities = np.array(list(partition.items()))
+    
+    # The resulting text file will have each node and its community label on a separate line, separated by a space.
+    
+    np.savetxt(file_name, communities, delimiter=' ', fmt='%s')
+
+    return partition, communities
 
 
 # Retrieving the community label of each node for the healthy cell line
 
-# Computing the partition using Louvain community detection
-# The partition dictionary will contain the community assignments for each node in the graph
-# The resolution parameter will change the size of the communities
-# By setting the random_state parameter to the same value each time you run the algorithm, you should obtain the same community assignments
-
-partition_h = community.best_partition(G_h, weight='weight', resolution = 1, random_state = 0))
-
-# Converting the community_assignments dictionary to a numpy array
-# Each tuple from partition_h.items() consists of a node as the key and its corresponding community label as the value.
-# The resulting array will have each tuple as a row, where the first column contains the node and the second column contains the community label.
-
-communities_h = np.array(list(partition_h.items()))
-
-# The resulting text file will have each node and its community label on a separate line, separated by a space.
-
-np.savetxt('communities_h.txt', communities_h, delimiter=' ', fmt='%s')
+partition_h, communities_h = community_label(G_h, 'communities_h.txt')
 
 
 # Retrieving the community label of each node for the cancer cell line
 
-# Computing the partition using Louvain community detection
-# The partition dictionary will contain the community assignments for each node in the graph
-# The resolution parameter will change the size of the communities
-# By setting the random_state parameter to the same value each time you run the algorithm, you should obtain the same community assignments
+partition_c, communities_c = community_label(G_c, 'communities_c.txt')
 
-partition_c = community.best_partition(G_c, weight='weight', resolution = 1, random_state = 0)
 
-# Converting the community_assignments dictionary to a numpy array
-# Each tuple from partition_h.items() consists of a node as the key and its corresponding community label as the value.
-# The resulting array will have each tuple as a row, where the first column contains the node and the second column contains the community label.
-
-communities_c = np.array(list(partition_c.items()))
-
-# The resulting text file will have each node and its community label on a separate line, separated by a space.
-
-np.savetxt('communities_c.txt', communities_c, delimiter=' ', fmt='%s')
