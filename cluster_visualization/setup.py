6ebb1432174e75913c39e8edf09c03974f80c0b0
majagrottum <138011685+majagrottum@users.noreply.github.com>
2023-07-19 16:58:50 +0200

    Created a setup.py file

---
+++ cluster_visualization/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'cluster_visualization',
+    version = '0.1.0',
+    packages = ['cluster_visualization'],
+    install_requires=[ 'numpy', 'networkx', 'node2vec', 'hdbscan', 'scikit-learn', 'matplotlib',],
+    entry_points = {
+        'console_scripts': [
+            'cluster_visualization = cluster_visualization.__main__:Visualization-of-clusters-in-undirected-weighted-network-graphs',
+        ]
+    })
