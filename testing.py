83feb5f37dfe6c7cfebb84b1214591e72b45a92e
majagrottum <138011685+majagrottum@users.noreply.github.com>
2023-07-21 15:28:49 +0200

    Edited the test_embedding_list function
    Removed the temporary file with sample node embeddings, and added a file_name as input parameter to the function instead

--- testing.py
+++ testing.py
@@ -174,7 +174,7 @@ def test_content_node_embeddings_file(graph, file_name, D, WL, NW, P, Q):
 
 
 
-def test_embedding_list():
+def test_embedding_list(file_name):
 
   """This function tests if the embedding_list function correctly retrieves the node embeddings from the specified file 
   and returns them in the appropriate format, which is a list where each element is a list with the embedding of a certain node. 
@@ -184,37 +184,28 @@ def test_embedding_list():
 
   """
 
-  # Creating a temporary file with sample node embeddings
-  with open("temp_node_embeddings.txt", "w") as file:
+  with open(file_name, 'r') as f:
     
-    file.write("5 64\n")
-    file.write("1 0.1 0.2 ... 0.64\n")
-    file.write("2 0.3 0.4 ... 0.67\n")
-    file.write("3 0.5 0.6 ... 0.68\n")
-    file.write("4 0.7 0.8 ... 0.69\n")
-    file.write("5 0.9 1.0 ... 0.70\n")
+        content = f.readlines()
 
-  # Using the temporary file to get the embedding list
-  file_name = "temp_node_embeddings.txt"
+  # Checking if the first row has the correct embedding information
+  first_row = content[0].split()
   
   embeddings = embedding_list(file_name)
 
-  # Checking if the returned list has the correct number of embeddings (should be 5)
-  assert len(embeddings) == 5, "Test failed: The number of embeddings in the list is incorrect."
+  # Checking if the returned list has the correct number of embeddings 
+  assert len(embeddings) == int(first_row[0]) ==, "Test failed: The number of embeddings in the list is incorrect."
 
-  # Checking if each element in the list is a list of coordinates with the correct length (should be 64)
+  # Checking if each element in the list is a list of coordinates with the correct length 
   for embedding in embeddings:
     
     assert isinstance(embedding, list), "Test failed: Each element in the list should be a list of coordinates."
       
-    assert len(embedding) == 64, "Test failed: The length of each embedding should be 64."
-
-  # Cleaning up: removing the temporary file
-  os.remove("temp_node_embeddings.txt")
+    assert len(embedding) == int(first_row[1]), "Test failed: The length of each embedding is incorrect."
 
   
 
-
+  
 
 def test_clustering_HDBSCAN_datatype(embedding):
 
