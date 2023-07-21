00624b8aff73ce17773f34d19a4a889fe9285421
majagrottum <138011685+majagrottum@users.noreply.github.com>
2023-07-21 11:49:24 +0200

    Added the test_nodes_and_edges function
    Added the function to test if the output graph have at least one node and a non-negative number of edges

--- testing.py
+++ testing.py
@@ -6,6 +6,17 @@ def test_create_graph_output_type(filename, splitting):
   
   assert isinstance(graph, nx.Graph), "Test failed: The output should be an instance of nx.Graph."
 
+
+def test_nodes_and_edges(filename, splitting):
+
+  """This function tests if the output graph have at least one node and a non-negative number of edges."""
+
+  graph = create_graph(filename, splitting)
+
+  assert graph.number_of_nodes() > 0, "Test failed: The graph should have at least one node."
+  
+  assert graph.number_of_edges() >= 0, "Test failed: The graph should have a non-negative number of edges."
+
 def test_remove_isolated_nodes():
 
 def test_node_embeddings():
