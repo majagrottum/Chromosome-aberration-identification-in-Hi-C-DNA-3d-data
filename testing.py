2b9d6e6da953acd5535ab7f4dfee3beb0f3415ee
majagrottum <138011685+majagrottum@users.noreply.github.com>
2023-07-21 11:22:40 +0200

    Added the function test_create_graph_output_type
    Added the test to test if the output of the create_graph function is an undirected graph or a subclass of nx.Graph

--- testing.py
+++ testing.py
@@ -1,4 +1,10 @@
-def test_create_graph():
+def test_create_graph_output_type(filename, splitting):
+
+  """This function tests if the output of the create_graph function is an undirected graph or a subclass of nx.Graph."""
+
+  graph = create_graph(filename, splitting)
+  
+  assert isinstance(graph, nx.Graph), "Test failed: The output should be an instance of nx.Graph."
 
 def test_remove_isolated_nodes():
 
